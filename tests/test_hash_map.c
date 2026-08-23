39ae86fcc48edbcf76408e5f01299f12218a075d
Pato <pheprogrammer@gmail.com>
2026-08-23 11:49:22 -0300

    test(hashmap): update init size | add test todo comments

--- tests/test_hash_map.c
+++ tests/test_hash_map.c
@@ -3,8 +3,16 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+// Test hash function shoud generate same value to same input
+// Test new hmap shoud iniciate pointers with correct start size
+// Test hmap insert shoud add key in the right place, key_idx on the right place and hmap value on the right place and header should update correctly
+// Test hmap insert should update correctly without incrementing the allocation size 
+// get keys should iterate correctly
+// get values should iterate correctly
+// get items should iterate correctly
+
 void test_hashmap_and_header_should_initialize_correctly() {
   int *h_map = NULL;
 
-  new_hmap(&h_map, 10);
+  new_hmap(&h_map, 32);
 }
